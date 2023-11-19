b532c375e70bc8f94af3b88383bbf44e9bfa7397
Cristian <cristian.cordoba01@uptc.edu.co>
2023-11-19 01:56:27 -0500

    Configuracion de los modelos de validacion para las peticiones

--- backInfotech/shemas/user.schema.js
+++ backInfotech/shemas/user.schema.js
@@ -1,34 +1,42 @@
 import {z} from 'zod'
 
+//validar que se registren todos los datos que se piden en el formulario de registro de usuarios
 export const registerUserShema = z.object({
     username: z.string({
-        required_error: 'name is required'
+        required_error: 'El nombre es requerido'
     }),
 
     lastname: z.string({
-        required_error: 'lastname is required'
+        required_error: 'El apellido es requirido'
     }),
 
     email: z.string({
-        required_error: "email is required"
+        required_error: 'El correo es requerido'
     }).email({
-        message: "invalid email"
+        message: 'El correo es invalido'
     }),
 
     password: z.string({
-        required_error: 'password is required'
+        required_error: 'La constraseña es requerida'
     }).min(8,{
-        message: 'password must be at least 8 characters long'
+        message: 'La constresaña debe tener mínimo 8 caracteres'
     }),
 
     address: z.string({
-        required_error: 'address is required'
+        required_error: 'La direccion es requerida'
     }),
 
-    role: z.string({
-        required_error: 'role is required'
+})
+
+//modelo para la peticion de login, de forma que se envien todos los datos necesarios
+export const loginUserSchema = z.object({
+    emial: z.string({
+        required_error: 'El correo es requerido'
+    }).email({
+        message: 'El correo es invalido'
+    }),
+
+    password: z.string({
+        required_error: 'Contraseña es requerida'
     })
-
-
-
 })
