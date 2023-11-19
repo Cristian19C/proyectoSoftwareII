c49fb4e8af0dd34c451ec9f5a18683a74547d2f2
Cristian <cristian.cordoba01@uptc.edu.co>
2023-11-19 01:55:41 -0500

    Configuracion de rutas

--- backInfotech/routes/user.routes.js
+++ backInfotech/routes/user.routes.js
@@ -1,15 +1,20 @@
 import { Router } from 'express'
 const router = Router()
 
+//funciones para la coleccion de usuarios
 import {obtainAllUsers, saveUser, findUserById, deleteUser, loginUser, sendMailRecoveryPass, updatePassword} from '../controllers/controll_user.js'
 
+//modelo para la validacion del cuerpo de las peticiones
+import { registerUserShema, loginUserSchema } from '../shemas/user.schema.js'
+//funcion para validar el modelo de las peticiones
+import { validateSchema } from '../middlewares/validatorUser.js'
 
-
+//configuracion de rutas para la parte de usuarios
 router.get('/', obtainAllUsers)
-router.post('/', saveUser)
+router.post('/registerUser', validateSchema(registerUserShema), saveUser)
 router.get('/:id', findUserById)
 router.delete('/:id', deleteUser)
-router.post('/login', loginUser)
+router.post('/login', validateSchema(loginUserSchema),loginUser)
 router.post('/updatePassword', updatePassword)
 router.post('/sendEmail/:email', sendMailRecoveryPass)
 
