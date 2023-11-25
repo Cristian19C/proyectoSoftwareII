0774d68350c4b5c803a070e4b86dad3a53f5b754
Cristian <cristian.cordoba01@uptc.edu.co>
2023-11-25 11:00:18 -0500

    Configutación de las rutas del aplicativo, referente al usuario

--- backInfotech/routes/user.routes.js
+++ backInfotech/routes/user.routes.js
@@ -1,13 +1,14 @@
-import { Router } from 'express'
+import {Router} from 'express'
 const router = Router()
 
 //funciones para la coleccion de usuarios
-import {obtainAllUsers, saveUser, findUserById, deleteUser, loginUser, sendMailRecoveryPass, updatePassword} from '../controllers/controll_user.js'
+import {obtainAllUsers, saveUser, findUserById, deleteUser, loginUser, sendMailRecoveryPass, updatePassword, profile, verifyToken} from '../controllers/controll_user.js'
 
 //modelo para la validacion del cuerpo de las peticiones
 import { registerUserShema, loginUserSchema } from '../shemas/user.schema.js'
 //funcion para validar el modelo de las peticiones
 import { validateSchema } from '../middlewares/validatorUser.js'
+import { authRequired } from '../middlewares/validateToken.js'
 
 //configuracion de rutas para la parte de usuarios
 router.get('/', obtainAllUsers)
@@ -17,5 +18,7 @@ router.delete('/:id', deleteUser)
 router.post('/login', validateSchema(loginUserSchema),loginUser)
 router.post('/updatePassword', updatePassword)
 router.post('/sendEmail/:email', sendMailRecoveryPass)
+router.post('verify', verifyToken)
+router.get('/profile', authRequired, profile)
 
-module.exports = router
+export default router
