04b2d44a34527687bea53c101fa9af752e670e2e
Cristian <cristian.cordoba01@uptc.edu.co>
2023-11-25 10:55:37 -0500

    Agregar dependencias y configuración para iniciar la aplicación

--- backInfotech/back.js
+++ backInfotech/back.js
@@ -1,22 +1,29 @@
-const express = require('express')
+import express from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+import connectToDatabase from './drivers/connect_bd.js';
+import userRoutes from './routes/user.routes.js'
 // const { auth,requiresAuth } = require('express-openid-connect');
 const app = express()
-const cors = require('cors')
 // const { swaggerUi, specs } = require('./swagger')
-require('dotenv').config()
+import dotenv from 'dotenv';
+dotenv.config();
+
 
 
 
 app.set('PORT', process.env.PORT)
 app.use(express.json())
+app.use(cookieParser())
 app.use(cors())
 // app.use(auth(config));
 
 app.listen(app.get('PORT'), ()=>{
     console.log(`Server listen to port: ${app.get('PORT')}` );
 })
-require('./drivers/connect_bd')
+
+connectToDatabase();
 
 
 
-app.use('/users',require('./routes/user.routes'))
+app.use('/users',userRoutes)
